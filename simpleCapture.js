b8c6aa1432af0cc11de6ddaf01d821da22463ec3
jon chambers <jon@jonchambers.net>
2018-11-05 10:50:10 -0600

    Add files via upload

--- simpleCapture.js
+++ simpleCapture.js
@@ -1,14 +1,15 @@
 var capture;
-
+var canvas;
 function setup() {
  
   capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  
+  capture.size(1920, 1080);
+  capture.hide();
+  canvas = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(255);
-  image(capture, 0, 0, 320, 240);
-
+  image(capture, 0, 0);
+  filter(GRAY);
 }
--- videoFile.js
+++ videoFile.js
@@ -1,26 +1,78 @@
 
 var T1000;
+var playButton;
+var vidPlaying = false;
+var videos = ['T1000 Reforming', 't2VidGame'];
+var vidSelect;
 
+var pulsar;
+var t2image;
 
+function preload(){
+ t2image = loadImage('images/t2.jpg');
+
+}
 
 function setup() {
-  // canvas = createCanvas(windowWidth, windowHeight);
-  // canvas.style('z-index', '-1');
-  T1000 = createVideo(['videos/mercuryT10002.mp4']);
+   canvas = createCanvas(windowWidth, windowHeight);
+   canvas.style('z-index', '-1');
+ 
+ pulsar = createAudio('audio/pulsar.mp3');
+ //pulsar.autoplay(true);
+ //pulsar.showControls();
+	pulsar.volume(.3);
+  T1000 = createVideo(['']);
+  T1000.hide();
   
-  
+  vidSelect = createSelect();
+  vidSelect.position(10, 10);
+  for(i = 0; i < videos.length; i++){
+  	vidSelect.option(videos[i]);
+  }
+  vidSelect.changed(changeVid);
+
+
+ playButton = createButton('Play Video');
+ playButton.mousePressed(playVid);
+ playButton.position(10, 35);
+	//T1000.loop();
+	
+}
+
+function changeVid(){
+	T1000.attribute('src', 'videos/' + vidSelect.value() + '.mp4');
+	T1000.position(150, 300);
+	T1000.show();
+}
+function playVid(){
+	if(vidPlaying == true){
+		T1000.pause();
+		playButton.html('Play Video');
+	} else {
+		T1000.play();
+		T1000.loop().time(10);
+		T1000.volume(.3);
+		//T1000.speed(2);
+		playButton.html('Pause Video');
+	}
+	vidPlaying = !vidPlaying;
 }
 
 function draw(){
- 
-if(mouseIsPressed == true){
-  T1000.play();
-T1000.loop();
-}else{
-  T1000.pause();
+ 	image(t2image, 0, 0, windowWidth, windowHeight );
+
+	T1000.position(mouseX, 300);
 }
 
+function windowResized(){
+	resizeCanvas(windwowidth, windowHeight);
 }
+
+
+
+
+
+
 
 
 
